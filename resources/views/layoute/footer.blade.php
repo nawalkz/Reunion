6fd2afd94ae1c2f9cb83f82c53cf94fc6d1efd43
kawtar loui <kawtarloui.202@gmail.com>
2025-05-18 19:21:00 +0200

    creation de front-end et separation de layout

---
+++ resources/views/layoute/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="bg-gray-100 text-center py-4 text-sm text-gray-600">
+    © 2025 Company Name. Tous droits réservés.  
+    <br>Contact : info@company.com | +212 6 66 66 66 66
+</footer>
---
+++ resources/views/layoute/script.blade.php
@@ -0,0 +1,4 @@
+<!-- Bootstrap JS Bundle CDN (inclut Popper.js) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs" defer></script>
+<script src="{{ asset('js/app.js') }}"></script>
